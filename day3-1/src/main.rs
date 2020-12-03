47bdc3f04df4f2fc0a94e36a1aa60619db813c3e
Robin Verlangen <robin+github@us2.nl>
2020-12-03 22:45:08 +0100

    too sleepy lol

--- day3-1/src/main.rs
+++ day3-1/src/main.rs
@@ -1,41 +1,106 @@
 use std::io::{stdout, Write};
 use std::env;
 
-use curl::easy::Easy;
+extern crate curl;
 
 fn main() {
 	let body = from_assignment(3);
+
+	let tree = '#';
+	let grass = '.';
+	let slopeX = 3;
+	let slopeY = 1;
+
+	// to multi dimensional grid
+	let lines = body.split("\n");
+	let numLines = body.split("\n").count();
+	// println!("{}", numLines);
+	let mut dimension = 0;
+	let mut desiredWidth = 0;
+	let mut grid = vec![vec!['#'; 0]; 0];
+	let mut y = 0;
+    for line in lines {
+    	let chars = line.chars();
+    	if dimension == 0 {
+	    	dimension = line.chars().count();
+	    	desiredWidth = numLines * slopeX;
+	    	grid = vec![vec!['#'; desiredWidth]; numLines];
+	    	println!("{}x{}", desiredWidth, numLines)
+	    }
+
+	    let mut x = 0;
+	    'outer: loop {
+		    for (j, s) in line.chars().into_iter().enumerate() {
+		    	// println!("x {} y {} = {} ({})", x, y, s, j);
+		    	if x >= desiredWidth {
+		    		break 'outer;
+				}
+				// print!("{}", s);
+		    	grid[y][x] = s;
+		    	x += 1;
+
+		    	// stop
+		    	// println!("a");
+		    }
+		    // println!("b {} {}", x, desiredWidth);
+		    if x == 0 {
+		    	// empty line
+		    	break;
+		    }
+		}
+		// print!("\n");
+        y += 1;
+    }
+
+    // start
+    println!("start");
+    let mut posX = 0;
+    let mut posY = 0;
+    let mut numTrees = 0;
+    loop {
+    	let val = grid[posY][posX];
+    	let isTree = val == tree;
+    	let isGrass = val == grass;
+    	println!("x{} y{} {} tree={} open={}", posX, posY, val, isTree, isGrass);
+
+    	if isTree {
+    		numTrees += 1;
+    	}
+
+    	// move
+    	posX += slopeX;
+    	posY += slopeY;
+
+    	// done?
+    	if posY >= numLines {
+    		break;
+    	}
+    }
+
+    println!("numTrees {}", numTrees)
+
 }
 
 pub fn from_assignment(day: i32) -> String {
-    let mut handle = Easy::new();
+    let mut data = Vec::new();
+    let mut easy = curl::easy::Easy::new();
     let session = env::var("ADVENT_SESSION").unwrap_or("".to_string());
-    // println!("{}", session);
-    let url = &format!("https://adventofcode.com/2020/day/{}/input", day);
-    handle.url(url).unwrap();
-    handle.write_function(|data| {
-        stdout().write_all(data).unwrap();
-        Ok(data.len())
-    }).unwrap();
-    
+    easy.url(&format!("https://adventofcode.com/2020/day/{}/input", day)).unwrap();
+
     // cookie
     let mut sess: String = "session=".to_owned();
     sess.push_str(&session);
-    // println!("{}", sess);
-    handle.cookie(&sess).unwrap();
-    handle.perform().unwrap();
-
-    // resp
-    let mut data = Vec::new();
+    easy.cookie(&sess).unwrap();
     {
-        let mut transfer = handle.transfer();
+        let mut transfer = easy.transfer();
         transfer.write_function(|new_data| {
             data.extend_from_slice(new_data);
             Ok(new_data.len())
         }).unwrap();
         transfer.perform().unwrap();
     }
+
+    // Convert it to `String`
     let body = String::from_utf8(data).expect("body is not valid UTF8!");
-    println!("{}", body);
     return body;
 }
